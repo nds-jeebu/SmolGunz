ca4c53d46e41c5f53444372fc83b70c8fe4bde1f
Muhammad Akbar <najeebakbar96@gmail.com>
2020-03-13 22:12:05 -0400

    look at me.

---
+++ lesson_1_summary.py
@@ -0,0 +1,7 @@
+# So, here's the first step towards building cool shit
+# We need to make it simple to communicate and share software
+
+print('1. git add .')
+print('2. git commit -m \"Some short message.\"')
+print('3. git push')
+
