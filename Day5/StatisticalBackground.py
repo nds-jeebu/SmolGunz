70dce8e829990c95d7026f0aa6ecba3ef38f01ab
Muhammad Akbar <najeebakbar96@gmail.com>
2020-03-18 21:35:56 -0400

    added file 4 u beb

---
+++ Day5/StatisticalBackground.py
@@ -0,0 +1,6 @@
+### import numpy and opencv
+
+### 1. read in all of the images into a list
+### Hint: a list is declared like a = [img1, img2, ...]
+### Hint: to add stuff to a list called l use l.append(stuff)
+
