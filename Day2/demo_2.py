ab9c8172c5d20c3b79e8f84356a98209676a7174
nds-jeebu <61555465+nds-jeebu@users.noreply.github.com>
2022-02-14 16:37:02 -0500

    Update demo_2.py

--- Day2/demo_2.py
+++ Day2/demo_2.py
@@ -5,7 +5,7 @@ import cv2 # our computer vision library
 ### Chunk 2: all of the functions
 
 
-### Chunk 3: real meat of the file -- i.e. where all of the shit happens
+### Chunk 3: real meat of the file -- i.e. where all of the stuff happens
 
 print('hi edward!!') # example of a function -- 
 # takes in a string and prints that string to terminal
